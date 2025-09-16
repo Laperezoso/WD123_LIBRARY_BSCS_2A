0652fd89e4847e4f00eb66899c06bd46694f5452
Razel_014 <razelherodias014@gmail.com>
2025-09-16 20:46:54 +0800

    Create database.php

---
+++ class/database.php
@@ -0,0 +1,15 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname = "LIBRARY";
+
+    protected $conn;
+
+    public function connect(){
+        $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
+        return $this->conn;
+    }
+}
+?>
