843c5adc6bf419b136e9fa691b38b8f93f42503b
Razel_014 <razelherodias014@gmail.com>
2025-09-23 21:05:00 +0800

    Update book.php

--- class/book.php
+++ class/book.php
@@ -16,42 +16,89 @@ class Book {
     }
 
     public function addBook(){
-        $sql = "INSERT INTO book (title, author, genre, pub_year) 
-                VALUES (:title, :author, :genre, :pub_year)";
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':title', $this->title);
-        $query->bindParam(':author', $this->author);
-        $query->bindParam(':genre', $this->genre);
-        $query->bindParam(':pub_year', $this->pub_year);
+    $sql = "INSERT INTO book (title, author, genre, pub_year) 
+            VALUES (:title, :author, :genre, :pub_year)";
+    $query = $this->db->connect()->prepare($sql);
+    $query->bindParam(':title', $this->title);
+    $query->bindParam(':author', $this->author);
+    $query->bindParam(':genre', $this->genre);
+    $query->bindParam(':pub_year', $this->pub_year);
 
-        return $query->execute();
-    }
+    return $query->execute();
+}
 
-    public function viewBook($search = ""){
-        $sql = "SELECT * FROM book WHERE title LIKE CONCAT('%', :search, '%') ORDER BY title ASC";
+
+    public function viewBook($genre="", $search="") {
+        $sql = "SELECT * FROM book WHERE title LIKE CONCAT('%', :search, '%')";
+        
+        if(!empty($genre)) { 
+            $sql .= " AND genre = :genre";
+        }
+
+        $sql .= "ORDER BY title ASC"; // ORDER the books
+
         $query = $this->db->connect()->prepare($sql);
         $query->bindParam(":search", $search);
 
+        if(!empty($genre)) { // bind genre
+            $query->bindParam(":genre", $genre);
+        }
         if($query->execute()){
             return $query->fetchAll();
+        }else{
+            return null;
+        }
+    }
+
+
+    public function isBookExist($title){
+        $sql = "SELECT COUNT(*) as total FROM book WHERE title = :title";
+        $query = $this->db->connect()->prepare($sql);
+        $query->bindParam(":title", $title);
+        $record = null;
+
+        if ($query->execute()) {
+            $record = $query->fetch();
+        }
+
+        return $record["total"] > 0;
+    }
+
+    public function fetchBook($b_id) {
+        $sql = "SELECT * FROM book WHERE id = :id";
+        $query = $this->db->connect()->prepare($sql);
+        $query->bindParam(":id", $b_id);
+
+        if ($query->execute()) {
+            return $query->fetch();
         } else {
             return null;
         }
     }
 
-     public function isBookExist($title){
-        $sql = "SELECT COUNT(*) as total FROM book WHERE title = :title";
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(":title", $title);
-        $record = null;
-        if ($query->execute()) {
-            $record = $query->fetch();
-        }
+    public function editBook($b_id) {
+        $sql = "UPDATE book 
+                SET title = :title, author = :author, genre = :genre, pub_year = :pub_year
+                WHERE id = :id";
 
-        if($record["total"] > 0){
-            return true;
-        }else{
-            return false;
-        }
+        $query = $this->db->connect()->prepare($sql);
+
+        $query->bindParam(":title", $this->title);
+        $query->bindParam(":author", $this->author);
+        $query->bindParam(":genre", $this->genre);
+        $query->bindParam(":pub_year", $this->pub_year);
+        
+        $query->bindParam(":id", $b_id);
+ 
+        return $query->execute();
     }
+
+   public function deleteBook($b_id) {
+    $sql = "DELETE FROM book WHERE id = :id";
+    $query = $this->db->connect()->prepare($sql);
+    $query->bindParam(":id", $b_id);
+
+    return $query->execute();
+}
+
 }
